f1bde4a6e000de9fcd7ec7bea8eb79ab2736090a
matthew <matthewyt765@gmail.com>
2025-03-15 16:41:26 +0800

    add about.php contacts.php home.php and footer.html

---
+++ home.php
@@ -0,0 +1,26 @@
+<?php
+    include("nav.html");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <div style="width: 400px;">
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque temporibus sunt ipsa ab neque vero quidem 
+            necessitatibus iure alias ipsum nesciunt amet maxime quibusdam, eligendi sequi cum consectetur sed possimus. <br>
+            this is a <b> home page</b>
+        </p>
+    </div>
+
+
+</body>
+</html>
+
+<?php
+    include("footer.html");
+?>
